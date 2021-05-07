940e48d09309e927329b71bb94a273426fe006f9
Rich Ercolani <rincebrain@gmail.com>
2021-05-07 00:57:21 -0400

    Simple fix for return codes.
    You know, I swear I had experimental reasons for how that return
    logic worked, but testing shows this setup passes all the same
    tests and then some, and who am I to argue with results?
    
    Signed-off-by: Rich Ercolani <rincebrain@gmail.com>

--- lib/libzfs_core/libzfs_core.c
+++ lib/libzfs_core/libzfs_core.c
@@ -787,10 +787,8 @@ lzc_send_resume_redacted(const char *snapname, const char *from, int fd,
 	// it seems that, in the cases where the thread gets EBADF,
 	// the original codepath returned no errors. So we'll do
 	// that too.
-	if (dumbstatus != 0 && dumbstatus != EBADF) {
-		err = 1;
-		// this seems really rude but what're ya gonna do
-		errno = dumbstatus;
+	if (dumbstatus != 0) {
+		err = dumbstatus;
 	}
 
 
