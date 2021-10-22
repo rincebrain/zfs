2063258e089927d4c42a80db4ae09a76bbd1fb43
Pawel Jakub Dawidek <pawel@dawidek.net>
2021-10-22 06:56:41 -0700

    Avoid accessing BRT on every block free.
    Currently the biggest concern with BRT is the need to check BRT table
    on every data block free - if the block is there we need to decrement
    counter, if it is not in there we simply free the block. In majority
    of cases the block won't be in BRT. If BRT won't fit into memory we
    will have the same problems as with deduplication.
    
    To address that create an array of reference counters for every vdev
    (vdev array). A single counter represents 1GB of vdev storage.
    Keep vdev arrays in memory: for 1PB vdev we need 8MB of memory.
    
    If a block is being referenced through BRT we also bump reference
    counter for the 1GB chunk of where the blocks starts. If we have a
    data block at offset 0 which is referenced 3 times, another data
    block at offset 131072 which is referenced 2 times and one more
    data block at offset 262144 which is referenced 4 times, we will
    have 9 (3+2+4) in the vdev array at index 0.
    
    When we sync transactions group and update BRT entries we also sync
    vdev arrays if needed, but the vdev arrays are never removed from memory.
    
    When we are about to free a block we check the vdev array. If the
    array element at index (blk_offset/1GB) is greater than 0 then it is
    possible that we may have a BRT entry for this block, so we look it up.
    If it is 0 then we are sure there is no BRT entry and we can free
    the block.
    
    For very large vdevs, vdev array might potentially span multiple blocks
    on disk. If we update just a single entry in vdev array it would be
    a waste to write entire array to the disk. It is not a huge deal -
    vdev array for 8TB vdev is 64kB, but still... To address that maintain
    in-memory bitmap where a single bit represents as many array elements as
    we can fit in a (1 << ashift) block. Any change to the vdev array will
    set a bit in the bitmap, so we know that this specific block of the vdev
    array is dirty and needs to be synced to the disk.
    This is partially implemented.
    
    Adding new vdevs is supported, so is growing vdevs.
    Removing vdevs and shrinking them is not yet supported.

--- module/zfs/brt.c
+++ module/zfs/brt.c
@@ -35,11 +35,78 @@
 #include <sys/arc.h>
 #include <sys/dsl_pool.h>
 #include <sys/dsl_scan.h>
+#include <sys/vdev_impl.h>
+#include <icp/include/sys/bitmap.h>	/* XXX */
 
 /*
  * BRT - Block Reference Table.
  */
 #define	BRT_OBJECT_ENTRIES_NAME	"net.dawidek:brt:entries"
+#define	BRT_OBJECT_VDEV_PREFIX	"net.dawidek:brt:vdev-"
+
+/*
+ * We divide each VDEV into 1GB chunks. Each chunk is represented in memory
+ * by a 64bit counter, thus 1TB VDEV requires 8kB of memory.
+ */
+#define	BRT_RANGE_CHUNK	(1024 * 1024 * 1024)
+/*
+ * We don't want to update the whole structure every time. Maintain bitmap
+ * of dirty blocks within the regions, so that single bit represents a
+ * block size of refcounts. For example if we have a 64TB vdev then all
+ * refcounts take 512kB of memory. If the block size if 4kB then we want to
+ * have 128 separate bits dirty bits. Each bit will represent change in one
+ * of 512 refcounts (4kB / sizeof(uint64_t)).
+ */
+#define	BRT_RANGE_SIZE_TO_NBLOCKS(size, blocksize)			\
+	(((size) - 1) / (blocksize) / sizeof(uint64_t) + 1)
+
+typedef struct brt_vdev_phys {
+	uint64_t	bvp_size;
+	uint64_t	bvp_count;
+	uint64_t	bvp_drefsize;
+	uint64_t	bvp_dsize;
+} brt_vdev_phys_t;
+
+typedef struct brt_vdev {
+	uint64_t	bv_vdev;
+	/*
+	 * Sum of all bv_refcount[]s.
+	 */
+	uint64_t	bv_count;
+	/*
+	 * Remember VDEV size, so we can reallocate if its size changes.
+	 */
+	uint64_t	bv_size;
+	/*
+	 * This is the array with all the refcounts (one refcount per 1GB).
+	 */
+	uint64_t	*bv_refcount;
+	/*
+	 * Object number in the MOS.
+	 */
+	uint64_t	bv_object;
+	/*
+	 * Disk space savings thanks to BRT.
+	 */
+	uint64_t	bv_drefsize;
+	uint64_t	bv_dsize;
+	/*
+	 * bv_refcount[] potentially can be a bit to big to sychronize it all
+	 * when we just changed few refcounts. The fields below allow us to
+	 * track updates to bv_refcount[] array since the last sync.
+	 * A single bit in the bv_bitmap represents as many refcounts as can
+	 * fit into a single brt_blocksize, where brt_blocksize is
+	 * (1 << spa->spa_min_ashift).
+	 * For example we have 65536 refcounts in the bv_refcount array
+	 * (so the whole array is 512kB). We updated bv_refcount[2] and
+	 * bv_refcount[5]. In that case only first bit in the bv_bitmap will
+	 * be set and we will write only first 4kB out of 512kB (assuming
+	 * spa_min_ashift is 12).
+	 */
+	boolean_t	bv_dirty;
+	ulong_t		*bv_bitmap;
+	uint64_t	bv_nblocks;
+} brt_vdev_t;
 
 /*
  * In-core brt
@@ -50,7 +117,12 @@ typedef struct brt {
 	spa_t		*brt_spa;
 	objset_t	*brt_os;
 	uint64_t	brt_object;
+	uint64_t	brt_blocksize;
+	uint64_t	brt_drefsize;
+	uint64_t	brt_dsize;
 	list_t		brt_pending;
+	brt_vdev_t	*brt_vdevs;
+	uint64_t	brt_nvdevs;
 } brt_t;
 
 /*
@@ -240,6 +312,409 @@ brt_phys_total_refcnt(const brt_entry_t *bre)
 	return (bre->bre_phys.brp_refcnt);
 }
 
+static void
+brt_vdev_dump(brt_t *brt)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid;
+
+	if (brt->brt_nvdevs == 0) {
+		printf("BRT empty\n");
+		return;
+	}
+
+	printf("BRT vdev dump:\n");
+	for (vdevid = 0; vdevid < brt->brt_nvdevs; vdevid++) {
+		uint64_t idx;
+
+		brtvd = &brt->brt_vdevs[vdevid];
+		printf("[vdevid=%ju/%ju] dirty=%d size=%ju count=%ju nblocks=%ju bitmapsize=%ju\n", (uintmax_t)vdevid, (uintmax_t)brtvd->bv_vdev, brtvd->bv_dirty, (uintmax_t)brtvd->bv_size, (uintmax_t)brtvd->bv_count, (uintmax_t)brtvd->bv_nblocks, (uintmax_t)BT_SIZEOFMAP(brtvd->bv_nblocks));
+		if (!brtvd->bv_dirty) {
+			continue;
+		}
+		printf("refcounts:\n");
+		for (idx = 0; idx < brtvd->bv_size; idx++) {
+			if (brtvd->bv_refcount[idx] > 0) {
+				printf("  [%04ju] %ju\n", (uintmax_t)idx, (uintmax_t)brtvd->bv_refcount[idx]);
+			}
+		}
+		printf("bitmap: ");
+		for (idx = 0; idx < brtvd->bv_nblocks; idx++) {
+			printf("%d", BT_TEST(brtvd->bv_bitmap, idx));
+		}
+		printf("\n");
+	}
+}
+
+static void
+brt_vdev_create(brt_t *brt, brt_vdev_t *brtvd, dmu_tx_t *tx)
+{
+	char name[64];
+
+	ASSERT(MUTEX_HELD(&brt->brt_lock));
+	ASSERT0(brtvd->bv_object);
+	ASSERT(brtvd->bv_refcount != NULL);
+	ASSERT(brtvd->bv_size > 0);
+	ASSERT(brtvd->bv_bitmap != NULL);
+	ASSERT(brtvd->bv_nblocks > 0);
+
+	/*
+	 * We allocate DMU buffer to store the bv_refcount[] array.
+	 * We will keep array size (bv_size) and cummulative count for all
+	 * bv_refcount[]s (bv_count) in the bonus buffer.
+	 */
+	brtvd->bv_object = dmu_object_alloc(brt->brt_os,
+	    DMU_OTN_UINT64_METADATA, brt->brt_blocksize,
+	    DMU_OTN_UINT64_METADATA, sizeof (brt_vdev_phys_t), tx);
+
+	snprintf(name, sizeof(name), "%s%ju", BRT_OBJECT_VDEV_PREFIX,
+	    (uintmax_t)brtvd->bv_vdev);
+	VERIFY0(zap_add(brt->brt_os, DMU_POOL_DIRECTORY_OBJECT, name,
+	    sizeof (uint64_t), 1, &brtvd->bv_object, tx));
+}
+
+static void
+brt_vdev_load(brt_t *brt, brt_vdev_t *brtvd)
+{
+	char name[64];
+	dmu_buf_t *db;
+	brt_vdev_phys_t *bvphys;
+	int error;
+
+	snprintf(name, sizeof(name), "%s%ju", BRT_OBJECT_VDEV_PREFIX,
+	    (uintmax_t)brtvd->bv_vdev);
+	error = zap_lookup(brt->brt_os, DMU_POOL_DIRECTORY_OBJECT, name,
+	    sizeof (uint64_t), 1, &brtvd->bv_object);
+	ASSERT(error == 0 || error == ENOENT);
+	if (error != 0)
+		return;
+
+	error = dmu_bonus_hold(brt->brt_os, brtvd->bv_object, FTAG, &db);
+	ASSERT0(error);
+	if (error != 0)
+		return;
+
+	bvphys = db->db_data;
+	/* TODO: We don't support VDEV shrinking. */
+	ASSERT3U(bvphys->bvp_size, <=, brtvd->bv_size);
+
+	/*
+	 * If VDEV grew, we will leave new bv_refcount[] entries untouched.
+	 */
+	error = dmu_read(brt->brt_os, brtvd->bv_object, 0,
+	    bvphys->bvp_size * sizeof (uint64_t), brtvd->bv_refcount,
+	    DMU_READ_NO_PREFETCH);
+	ASSERT0(error);
+
+	brtvd->bv_count = bvphys->bvp_count;
+	brtvd->bv_drefsize = bvphys->bvp_drefsize;
+	brtvd->bv_dsize = bvphys->bvp_dsize;
+	brt->brt_drefsize += brtvd->bv_drefsize;
+	brt->brt_dsize += brtvd->bv_dsize;
+
+	dmu_buf_rele(db, FTAG);
+}
+
+static void
+brt_vdev_destroy(brt_t *brt, brt_vdev_t *brtvd, dmu_tx_t *tx)
+{
+	char name[64];
+	dmu_buf_t *db;
+	brt_vdev_phys_t *bvphys;
+
+	VERIFY0(dmu_bonus_hold(brt->brt_os, brtvd->bv_object, FTAG, &db));
+	bvphys = db->db_data;
+	ASSERT0(bvphys->bvp_count);
+	ASSERT0(bvphys->bvp_drefsize);
+	ASSERT0(bvphys->bvp_dsize);
+	dmu_buf_rele(db, FTAG);
+
+	ASSERT0(dmu_object_free(brt->brt_os, brtvd->bv_object, tx));
+
+	snprintf(name, sizeof(name), "%s%ju", BRT_OBJECT_VDEV_PREFIX,
+	    (uintmax_t)brtvd->bv_vdev);
+	VERIFY0(zap_remove(brt->brt_os, DMU_POOL_DIRECTORY_OBJECT, name, tx));
+
+	brtvd->bv_object = 0;
+}
+
+static void
+brt_vdev_sync_one(brt_t *brt, brt_vdev_t *brtvd, dmu_tx_t *tx)
+{
+	dmu_buf_t *db;
+	brt_vdev_phys_t *bvphys;
+
+	ASSERT(brtvd->bv_dirty);
+
+	if (brtvd->bv_object == 0) {
+		/*
+		 * No BRT VDEV object for this VDEV yet, allocate one.
+		 */
+		brt_vdev_create(brt, brtvd, tx);
+	}
+
+	ASSERT(dmu_tx_is_syncing(tx));
+
+	VERIFY0(dmu_bonus_hold(brt->brt_os, brtvd->bv_object, FTAG, &db));
+
+	/*
+	 * TODO: Walk through brtvd->bv_bitmap and write only dirty parts.
+	 */
+	dmu_write(brt->brt_os, brtvd->bv_object, 0,
+	    brtvd->bv_size * sizeof (uint64_t), brtvd->bv_refcount, tx);
+
+	dmu_buf_will_dirty(db, tx);
+	bvphys = db->db_data;
+	bvphys->bvp_size = brtvd->bv_size;
+	bvphys->bvp_count = brtvd->bv_count;
+	bvphys->bvp_drefsize = brtvd->bv_drefsize;
+	bvphys->bvp_dsize = brtvd->bv_dsize;
+	dmu_buf_rele(db, FTAG);
+
+	bzero(brtvd->bv_bitmap, BT_SIZEOFMAP(brtvd->bv_nblocks));
+	brtvd->bv_dirty = FALSE;
+}
+
+static void
+brt_vdev_realloc(brt_t *brt, brt_vdev_t *brtvd)
+{
+	vdev_t *vd;
+	uint64_t *refcount;
+	ulong_t *bitmap;
+	uint64_t nblocks, size;
+
+	ASSERT(MUTEX_HELD(&brt->brt_lock));
+
+	spa_config_enter(brt->brt_spa, SCL_VDEV, FTAG, RW_READER);
+	vd = vdev_lookup_top(brt->brt_spa, brtvd->bv_vdev);
+	size = vdev_get_min_asize(vd) / BRT_RANGE_CHUNK;
+	spa_config_exit(brt->brt_spa, SCL_VDEV, FTAG);
+
+	refcount = kmem_zalloc(sizeof(uint64_t) * size, KM_SLEEP);
+	nblocks = BRT_RANGE_SIZE_TO_NBLOCKS(size, brt->brt_blocksize);
+	bitmap = kmem_zalloc(BT_SIZEOFMAP(nblocks), KM_SLEEP);
+
+	if (brtvd->bv_size == 0) {
+		ASSERT(brtvd->bv_refcount == NULL);
+		ASSERT(brtvd->bv_bitmap == NULL);
+		ASSERT0(brtvd->bv_nblocks);
+	} else {
+		ASSERT(brtvd->bv_refcount != NULL);
+		ASSERT(brtvd->bv_bitmap != NULL);
+		ASSERT(brtvd->bv_nblocks > 0);
+		/*
+		 * TODO: Allow vdev shrinking. We only need to implement
+		 * shrinking the on-disk BRT VDEV object.
+		 * dmu_free_range(brt->brt_os, brtvd->bv_object, offset, size, tx);
+		 */
+		ASSERT3U(brtvd->bv_size, <=, size);
+
+		bcopy(brtvd->bv_refcount, refcount,
+		    sizeof(uint64_t) * MIN(size, brtvd->bv_size));
+		bcopy(brtvd->bv_bitmap, bitmap,
+		    MIN(BT_SIZEOFMAP(nblocks), BT_SIZEOFMAP(brtvd->bv_nblocks)));
+		kmem_free(brtvd->bv_refcount,
+		    sizeof(uint64_t) * brtvd->bv_size);
+		kmem_free(brtvd->bv_bitmap, BT_SIZEOFMAP(brtvd->bv_nblocks));
+	}
+
+	brtvd->bv_size = size;
+	brtvd->bv_refcount = refcount;
+	brtvd->bv_bitmap = bitmap;
+	brtvd->bv_nblocks = nblocks;
+}
+
+static void
+brt_expand_vdevs(brt_t *brt, uint64_t nvdevs)
+{
+	brt_vdev_t *vdevs;
+	uint64_t vdevid;
+
+	ASSERT(MUTEX_HELD(&brt->brt_lock));
+	ASSERT3U(nvdevs, >, brt->brt_nvdevs);
+
+	vdevs = kmem_zalloc(sizeof(brt_vdev_t) * nvdevs, KM_SLEEP);
+	if (brt->brt_nvdevs > 0) {
+		ASSERT(brt->brt_vdevs != NULL);
+
+		bcopy(brt->brt_vdevs, vdevs,
+		    sizeof(brt_vdev_t) * brt->brt_nvdevs);
+		kmem_free(brt->brt_vdevs,
+		    sizeof(brt_vdev_t) * brt->brt_nvdevs);
+	}
+	for (vdevid = brt->brt_nvdevs; vdevid < nvdevs; vdevid++) {
+		vdevs[vdevid].bv_vdev = vdevid;
+	}
+
+	brt->brt_vdevs = vdevs;
+	brt->brt_nvdevs = nvdevs;
+}
+
+static boolean_t
+brt_vdev_lookup(brt_t *brt, const brt_entry_t *bre)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid, idx;
+	boolean_t found, unlock;
+
+	if (!MUTEX_HELD(&brt->brt_lock)) {
+		unlock = TRUE;
+		brt_enter(brt);
+	} else {
+		unlock = FALSE;
+	}
+
+	found = FALSE;
+
+	vdevid = bre->bre_key.brk_vdev;
+	idx = bre->bre_key.brk_offset / BRT_RANGE_CHUNK;
+
+	if (vdevid >= brt->brt_nvdevs)
+		goto out;
+
+	brtvd = &brt->brt_vdevs[vdevid];
+	if (brtvd->bv_refcount == NULL || idx >= brtvd->bv_size) {
+		/* VDEV must have been expanded. */
+		goto out;
+	}
+
+	found = brtvd->bv_refcount[idx] > 0;
+out:
+	if (unlock)
+		brt_exit(brt);
+
+	return (found);
+}
+
+static void
+brt_vdev_addref(brt_t *brt, const brt_entry_t *bre, uint64_t dsize)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid, idx;
+
+	ASSERT(MUTEX_HELD(&brt->brt_lock));
+
+	vdevid = bre->bre_key.brk_vdev;
+	idx = bre->bre_key.brk_offset / BRT_RANGE_CHUNK;
+
+	if (vdevid >= brt->brt_nvdevs) {
+		/* New VDEV was added. */
+		brt_expand_vdevs(brt, vdevid + 1);
+	}
+	ASSERT3U(vdevid, <, brt->brt_nvdevs);
+
+	brtvd = &brt->brt_vdevs[vdevid];
+	if (brtvd->bv_refcount == NULL || idx >= brtvd->bv_size) {
+		/* VDEV not allocated/loaded yet or was been expanded. */
+		brt_vdev_realloc(brt, brtvd);
+	}
+
+	ASSERT3U(idx, <, brtvd->bv_size);
+
+	brtvd->bv_count++;
+	brtvd->bv_refcount[idx]++;
+	brtvd->bv_dirty = TRUE;
+	idx = idx / brt->brt_blocksize / 8;
+	BT_SET(brtvd->bv_bitmap, idx);
+
+	brtvd->bv_dsize += dsize;
+	brt->brt_dsize += dsize;
+	if (brt_phys_total_refcnt(bre) == 1) {
+		brtvd->bv_drefsize += dsize;
+		brt->brt_drefsize += dsize;
+	}
+brt_vdev_dump(brt);
+}
+
+static void
+brt_vdev_decref(brt_t *brt, const brt_entry_t *bre, uint64_t dsize)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid, idx;
+
+	ASSERT(MUTEX_HELD(&brt->brt_lock));
+
+	vdevid = bre->bre_key.brk_vdev;
+	idx = bre->bre_key.brk_offset / BRT_RANGE_CHUNK;
+
+	ASSERT3U(vdevid, <, brt->brt_nvdevs);
+
+	brtvd = &brt->brt_vdevs[vdevid];
+	ASSERT(brtvd->bv_refcount != NULL);
+	ASSERT3U(idx, <, brtvd->bv_size);
+
+	ASSERT(brtvd->bv_count > 0);
+	brtvd->bv_count--;
+	ASSERT(brtvd->bv_refcount[idx] > 0);
+	brtvd->bv_refcount[idx]--;
+	brtvd->bv_dirty = TRUE;
+	idx = idx / brt->brt_blocksize / 8;
+	BT_SET(brtvd->bv_bitmap, idx);
+
+	brtvd->bv_dsize -= dsize;
+	brt->brt_dsize -= dsize;
+	if (brt_phys_total_refcnt(bre) == 0) {
+		brtvd->bv_drefsize -= dsize;
+		brt->brt_drefsize -= dsize;
+	}
+brt_vdev_dump(brt);
+}
+
+static void
+brt_vdevs_sync(brt_t *brt, dmu_tx_t *tx)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid;
+
+	ASSERT(MUTEX_HELD(&brt->brt_lock));
+
+	for (vdevid = 0; vdevid < brt->brt_nvdevs; vdevid++) {
+		brtvd = &brt->brt_vdevs[vdevid];
+		if (brtvd->bv_dirty)
+			brt_vdev_sync_one(brt, brtvd, tx);
+		if (brtvd->bv_count == 0)
+			brt_vdev_destroy(brt, brtvd, tx);
+	}
+}
+
+static void
+brt_vdevs_load(brt_t *brt)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid;
+
+	brt_enter(brt);
+
+	brt_expand_vdevs(brt, brt->brt_spa->spa_root_vdev->vdev_children);
+	for (vdevid = 0; vdevid < brt->brt_nvdevs; vdevid++) {
+		brtvd = &brt->brt_vdevs[vdevid];
+		ASSERT(brtvd->bv_refcount == NULL);
+
+		brt_vdev_realloc(brt, brtvd);
+		brt_vdev_load(brt, brtvd);
+	}
+
+	brt_exit(brt);
+}
+
+static void
+brt_vdevs_free(brt_t *brt)
+{
+	brt_vdev_t *brtvd;
+	uint64_t vdevid;
+
+	brt_enter(brt);
+
+	for (vdevid = 0; vdevid < brt->brt_nvdevs; vdevid++) {
+		brtvd = &brt->brt_vdevs[vdevid];
+		kmem_free(brtvd->bv_refcount,
+		    sizeof (uint64_t) * brtvd->bv_size);
+	}
+	kmem_free(brt->brt_vdevs, sizeof (brt_vdev_t) * brt->brt_nvdevs);
+
+	brt_exit(brt);
+}
+
 static boolean_t
 brt_object_exists(brt_t *brt)
 {
@@ -310,6 +785,9 @@ brt_object_lookup(brt_t *brt, brt_entry_t *bre)
 	if (!brt_object_exists(brt))
 		return (SET_ERROR(ENOENT));
 
+	if (!brt_vdev_lookup(brt, bre))
+		return (SET_ERROR(ENOENT));
+
 	return (brt_zap_lookup(brt->brt_os, brt->brt_object, bre));
 }
 
@@ -349,6 +827,7 @@ boolean_t
 brt_may_exists(spa_t *spa, const blkptr_t *bp)
 {
 	brt_t *brt = spa->spa_brt;
+	brt_entry_t bre_search;
 
 	if (!avl_is_empty(&brt->brt_tree))
 		return (TRUE);
@@ -356,7 +835,9 @@ brt_may_exists(spa_t *spa, const blkptr_t *bp)
 	if (!brt_object_exists(brt))
 		return (FALSE);
 
-	return (TRUE);
+	brt_key_fill(&bre_search.bre_key, bp);
+
+	return (brt_vdev_lookup(brt, &bre_search));
 }
 
 uint64_t
@@ -475,6 +956,7 @@ brt_entry_addref(brt_t *brt, const brt_key_t *brk, uint64_t dsize)
 			avl_insert(&brt->brt_tree, bre, where);
 	}
 	brt_phys_addref(&bre->bre_phys);
+	brt_vdev_addref(brt, bre, dsize);
 }
 
 /* Return TRUE if block should be freed immediately. */
@@ -545,6 +1027,7 @@ out:
 		BRTSTAT_BUMP(brt_decref_free_data_later);
 	else
 		BRTSTAT_BUMP(brt_decref_entry_still_referenced);
+	brt_vdev_decref(brt, bre, bp_get_dsize(brt->brt_spa, bp));
 
 	brt_exit(brt);
 
@@ -665,6 +1148,8 @@ brt_create(spa_t *spa)
 	brt->brt_spa = spa;
 	brt->brt_os = spa->spa_meta_objset;
 	brt->brt_blocksize = (1 << spa->spa_min_ashift);
+	brt->brt_vdevs = NULL;
+	brt->brt_nvdevs = 0;
 	brt_table_alloc(brt);
 
 	spa->spa_brt = brt;
@@ -681,6 +1166,9 @@ brt_load(spa_t *spa)
 	if (error != 0 && error != ENOENT)
 		return (error);
 
+	if (error == 0)
+		brt_vdevs_load(spa->spa_brt);
+
 	return (0);
 }
 
@@ -691,6 +1179,7 @@ brt_unload(spa_t *spa)
 
 	ASSERT(brt != NULL);
 
+	brt_vdevs_free(brt);
 	brt_table_free(brt);
 	mutex_destroy(&brt->brt_lock);
 	kmem_free(brt, sizeof(*brt));
@@ -755,6 +1244,8 @@ brt_sync_table(brt_t *brt, dmu_tx_t *tx, uint64_t txg)
 		if (count == 0)
 			brt_object_destroy(brt, tx);
 	}
+
+	brt_vdevs_sync(brt, tx);
 }
 
 void
