c2f3b66716a156b076a4348234eed73c66e1f4fc
Tino Reichardt <milky-zfs@mcmilk.de>
2022-03-05 12:08:05 +0100

    Add new files for BLAKE3
    This commit adds the cryptographic hash function BLAKE3 to the icp
    module.
    
    The code for this hash was put into public domain - so I could use the
    CDDL for all files.
    
    Homepage of BLAKE3: https://github.com/BLAKE3-team/BLAKE3
    
    The new code is mainly splitted into these parts:
    - 1x cross platform generic c variant: blake3_generic.c
    - 4x assembly for X86-64 (SSE3, SSE41, AVX2, AVX512)
    - 2x assembly for ARMv8 (NEON converted from SSE)
    - 2x assembly for PPC64 (POWER7 converted from SSE)
    - 2x assembly for PPC64LE (POWER7 converted from SSE)
    - one file for switching between the implementations
    
    Signed-off-by: Tino Reichardt <milky-zfs@mcmilk.de>

---
+++ module/icp/algs/blake3/blake3_impl.c
@@ -0,0 +1,265 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright (c) 2021-2022 Tino Reichardt <milky-zfs@mcmilk.de>
+ */
+
+#include <sys/zfs_context.h>
+#include <sys/zio_checksum.h>
+
+#include "blake3_impl.h"
+
+static const blake3_impl_ops_t *const blake3_impls[] = {
+	&blake3_generic_impl,
+#if defined(__aarch64__)
+	&blake3_sse2_impl,
+	&blake3_sse41_impl,
+#endif
+#if defined(__powerpc__)
+	&blake3_sse2_impl,
+	&blake3_sse41_impl,
+#endif
+#if defined(__x86_64) && defined(HAVE_SSE2)
+	&blake3_sse2_impl,
+#endif
+#if defined(__x86_64) && defined(HAVE_SSE4_1)
+	&blake3_sse41_impl,
+#endif
+};
+
+/*
+ * these wo seem to make some stress :/
+ *
+ * #if defined(__x86_64) && defined(HAVE_SSE4_1) && defined(HAVE_AVX2)
+ *	&blake3_avx2_impl,
+ * #endif
+ * #if defined(__x86_64) && defined(HAVE_AVX512F) && defined(HAVE_AVX512VL)
+ *	&blake3_avx512_impl,
+ * #endif
+ */
+
+/* this pointer holds current ops for implementation */
+static const blake3_impl_ops_t *blake3_selected_impls = &blake3_generic_impl;
+
+/* special implementation selections */
+#define	IMPL_FASTEST	(UINT32_MAX)
+#define	IMPL_CYCLE	(UINT32_MAX-1)
+#define	IMPL_USER	(UINT32_MAX-2)
+static uint32_t icp_blake3_impl = IMPL_FASTEST;
+
+#define	BLAKE3_IMPL_NAME_MAX	16
+
+/* id of fastest implementation */
+static uint32_t blake3_fastest_impl = 0;
+
+/* currently used implementation */
+static uint32_t blake3_current_impl = 0;
+
+/* return number of supported implementations */
+int
+blake3_get_impl_count(void)
+{
+	static int impls = 0;
+	int i;
+
+	if (impls)
+		return (impls);
+
+	for (i = 0; i < ARRAY_SIZE(blake3_impls); i++)
+		if (blake3_impls[i]->is_supported()) impls++;
+
+	return (impls);
+}
+
+/* return id of selected implementation */
+int
+blake3_get_impl_id(void)
+{
+	return (blake3_current_impl);
+}
+
+/* return name of selected implementation */
+const char *
+blake3_get_impl_name(void)
+{
+	return (blake3_selected_impls->name);
+}
+
+/* setup id as fastest implementation */
+void
+blake3_set_impl_fastest(uint32_t id)
+{
+	blake3_fastest_impl = id;
+}
+
+/* set implementation by id */
+void
+blake3_set_impl_id(uint32_t id)
+{
+	int i, impl;
+
+	/* select fastest */
+	if (id == IMPL_FASTEST) {
+		blake3_current_impl = blake3_fastest_impl;
+		blake3_selected_impls = blake3_impls[blake3_current_impl];
+		return;
+	}
+
+	/* select next or first */
+	if (id == IMPL_CYCLE) {
+		impl = (++blake3_current_impl) % blake3_get_impl_count();
+		blake3_current_impl = impl;
+		blake3_selected_impls = blake3_impls[impl];
+		return;
+	}
+
+	/* 0..N for the real impl */
+	for (i = 0, impl = 0; i < ARRAY_SIZE(blake3_impls); i++) {
+		if (!blake3_impls[i]->is_supported()) continue;
+		if (impl == id) {
+			blake3_current_impl = id;
+			blake3_selected_impls = blake3_impls[id];
+			return;
+		}
+		impl++;
+	}
+}
+
+/* set implementation by name */
+int
+blake3_set_impl_name(const char *name)
+{
+	int i, impl;
+
+	if (strcmp(name, "fastest") == 0) {
+		icp_blake3_impl = IMPL_FASTEST;
+		blake3_set_impl_id(IMPL_FASTEST);
+		return (0);
+	} else if (strcmp(name, "cycle") == 0) {
+		icp_blake3_impl = IMPL_CYCLE;
+		blake3_set_impl_id(IMPL_CYCLE);
+		return (0);
+	}
+
+	icp_blake3_impl = IMPL_USER;
+	for (i = 0, impl = 0; i < ARRAY_SIZE(blake3_impls); i++) {
+		if (!blake3_impls[i]->is_supported()) continue;
+		if (strcmp(name, blake3_impls[i]->name) == 0) {
+			blake3_selected_impls = blake3_impls[impl];
+			blake3_current_impl = impl;
+			return (0);
+		}
+		impl++;
+	}
+
+	return (-EINVAL);
+}
+
+/* setup implementation */
+void
+blake3_setup_impl(void)
+{
+	switch (icp_blake3_impl) {
+	case IMPL_FASTEST:
+		blake3_set_impl_id(IMPL_FASTEST);
+		break;
+	case IMPL_CYCLE:
+		blake3_set_impl_id(IMPL_CYCLE);
+		break;
+	default:
+		blake3_set_impl_id(blake3_current_impl);
+		break;
+	}
+}
+
+/* return selected implementation */
+const blake3_impl_ops_t *
+blake3_impl_get_ops(void)
+{
+	/* each call to ops will cycle */
+	if (icp_blake3_impl == IMPL_CYCLE)
+		blake3_set_impl_id(IMPL_CYCLE);
+
+	return (blake3_selected_impls);
+}
+
+#ifdef _KERNEL
+EXPORT_SYMBOL(blake3_get_impl_count);
+EXPORT_SYMBOL(blake3_get_impl_name);
+EXPORT_SYMBOL(blake3_set_impl_fastest);
+EXPORT_SYMBOL(blake3_set_impl_id);
+EXPORT_SYMBOL(blake3_set_impl_name);
+EXPORT_SYMBOL(blake3_setup_impl);
+#endif
+
+#if defined(_KERNEL) && defined(__linux__)
+static int
+icp_blake3_impl_set(const char *name, zfs_kernel_param_t *kp)
+{
+	char req_name[BLAKE3_IMPL_NAME_MAX];
+	size_t i;
+
+	/* sanitize input */
+	i = strnlen(name, BLAKE3_IMPL_NAME_MAX);
+	if (i == 0 || i >= BLAKE3_IMPL_NAME_MAX)
+		return (-EINVAL);
+
+	strlcpy(req_name, name, BLAKE3_IMPL_NAME_MAX);
+	while (i > 0 && isspace(req_name[i-1]))
+		i--;
+	req_name[i] = '\0';
+
+	return (blake3_set_impl_name(req_name));
+}
+
+static int
+icp_blake3_impl_get(char *buffer, zfs_kernel_param_t *kp)
+{
+	int i, impl, cnt = 0;
+	char *fmt;
+
+	/* cycling */
+	fmt = (icp_blake3_impl == IMPL_CYCLE) ? "[cycle] " : "cycle ";
+	cnt += sprintf(buffer + cnt, fmt);
+
+	/* fastest one */
+	fmt = (icp_blake3_impl == IMPL_FASTEST) ? "[fastest] " : "fastest ";
+	cnt += sprintf(buffer + cnt, fmt);
+
+	/* user selected */
+	for (i = 0, impl = 0; i < ARRAY_SIZE(blake3_impls); i++) {
+		if (!blake3_impls[i]->is_supported()) continue;
+		fmt = (icp_blake3_impl == IMPL_USER &&
+		    impl == blake3_current_impl) ? "[%s] " : "%s ";
+		cnt += sprintf(buffer + cnt, fmt, blake3_impls[i]->name);
+		impl++;
+	}
+
+	buffer[cnt] = 0;
+
+	return (cnt);
+}
+
+module_param_call(icp_blake3_impl, icp_blake3_impl_set, icp_blake3_impl_get,
+    NULL, 0644);
+MODULE_PARM_DESC(icp_blake3_impl, "Select BLAKE3 implementation.");
+#endif
